e1f4d5fc7eb584f24bf1b90e9354e7fafc5bf6a0
Tanay <90561803+Tanay-ErrorCode@users.noreply.github.com>
2024-06-02 11:26:22 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,24 +1,6 @@
 import pygame
 print('py')
 print('python')
-print('python3')
-print('java')
-print('js')
-print('javascript')
-print('c++')
-
-print('kotlin')
-print('kotlin')
-'''added new kotlin'''
-123
-
-print("k jio")
-123*234
-print("hi")*8
-
-
-print("new line hai ye")
-#8here @ 8:33
-
+print("done:")
 for i in range(10):
   print(i)*i
